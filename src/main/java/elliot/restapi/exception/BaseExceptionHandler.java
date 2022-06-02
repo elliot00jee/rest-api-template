dbe8565db107f1ad063f08233b2302084555eeda
elliot00jee <elliot00jee@gmail.com>
2022-06-02 23:03:52 +0900

    Refactoring - ApiResponse를 ResponseEntity로 감싸서 리턴하도록 변경

--- src/main/java/elliot/restapi/exception/BaseExceptionHandler.java
+++ src/main/java/elliot/restapi/exception/BaseExceptionHandler.java
@@ -7,13 +7,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.validation.ConstraintViolationException;
-
-import static elliot.restapi.util.ResponseUtils.ApiResponse;
 import static elliot.restapi.util.ResponseUtils.error;
 
 /**
- * @ExceptionHandler와 함께 사용하여 예외 처리를 모든 컨트롤러 전반에 걸쳐 적용(※ 적용 범위 설정 가능)
+ * ExceptionHandler와 함께 사용하여 예외 처리를 모든 컨트롤러 전반에 걸쳐 적용(※ 적용 범위 설정 가능)
  * 처리하고 싶은 RuntimeException 예외를 이 클래스에 등록하여 사용한다.
  */
 @Slf4j
@@ -39,7 +36,6 @@ public class BaseExceptionHandler {
     @ExceptionHandler({
             IllegalArgumentException.class,
             IllegalStateException.class,
-            ConstraintViolationException.class,
             GeneralBusinessException.class
     })
     public ResponseEntity<?> handleBadRequestException(Exception e) {
@@ -52,8 +48,7 @@ public class BaseExceptionHandler {
         return getResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
     private ResponseEntity<?> getResponse(String message, HttpStatus status) {
-        return new ResponseEntity<ApiResponse<?>>(error(message, status), status);
+        return error(message, status);
     }
 }
--- src/main/java/elliot/restapi/util/ResponseUtils.java
+++ src/main/java/elliot/restapi/util/ResponseUtils.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResponseUtils {
     @Getter
@@ -15,7 +19,6 @@ public class ResponseUtils {
         private final ResultCd resultCd;
         private final T data;
         private final String resultMessage;
-        private final int status;
     }
 
     public enum ResultCd {S, BE}
@@ -24,15 +27,31 @@ public class ResponseUtils {
      * 컨트롤러 단에서 정상 응답 보낼 때 호출
      * @param response data 필드에 넣어 반환할 객체
      */
-    public static <T> ApiResponse<T> success(T response) {
-        return new ApiResponse<>(ResultCd.S, response, null, HttpStatus.OK.value());
+    public static <T> ResponseEntity<?> success(T response) {
+        return new ResponseEntity<>(
+                new ApiResponse<>(ResultCd.S, response, null),
+                HttpStatus.OK);
     }
+
+    public static ResponseEntity<?> success() {
+        return success(null);
+    }
+
+    public static ResponseEntity<?> success(String key, String value) {
+        Map<String, String> response = new HashMap<>();
+        response.put(key, value);
+
+        return success(response);
+    }
+
     /**
      * 컨트롤러 단에서 에러 응답 보낼 때 호출
      * 직접 호출하지 않고, 예외 발생 시 @ExceptionHandler에서 처리
      */
-    public static ApiResponse<?> error(String resultMessage, HttpStatus status) {
-        return new ApiResponse<>(ResultCd.BE, null, resultMessage, status.value());
+    public static ResponseEntity<?> error(String resultMessage, HttpStatus errorStatus) {
+        return new ResponseEntity<>(
+                new ApiResponse<>(ResultCd.BE, null, resultMessage),
+                errorStatus);
     }
 
 }
